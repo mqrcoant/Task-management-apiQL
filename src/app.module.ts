da93044b6daece0682a92889277343936976e3f3
mqrcoo <marcoa13310@gmail.com>
2026-07-22 17:16:04 -0400

    feat: configura GraphQL, TypeORM y SQLite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Configuración de GraphQL (Enfoque Code-First)
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      // Generará el esquema automáticamente en esta ruta
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    // Configuración de la Base de Datos SQLite
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      autoLoadEntities: true, // Carga las entidades automáticamente sin declararlas una por una
+      synchronize: true, // Sincroniza la DB con tus entidades (ideal para desarrollo local)
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
